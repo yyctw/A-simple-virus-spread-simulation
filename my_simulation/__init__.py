4d1cb34019d160d4d53368e94a757c974d54e165
Eric860730 <s12345d60@gmail.com>
2021-12-28 11:37:31 +0800

    Update __init__.py

--- my_simulation/__init__.py
+++ my_simulation/__init__.py
@@ -1,3 +1,7 @@
-from my_simulation.simulator import simulate
+from my_simulation.simulator import run
 from my_simulation.visualize import build_figure
+from my_simulation.visualize import run_and_build_animation
+from my_simulation.visualize import draw_current_simulation_state
+from my_simulation.visualize import print_in_terminal
+from my_simulation.population import population
 __version__ = '0.1.0'
