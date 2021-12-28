3888cc83826de848d1e150e5aa71262573bba68a
Eric860730 <s12345d60@gmail.com>
2021-12-28 11:35:30 +0800

    Refactor to observe pattern and add annotate.

--- main.py
+++ main.py
@@ -1,32 +1,94 @@
-from config import config
+import os
+import matplotlib.pyplot as plt
+import time
+
+import config
 import my_simulation
 import _simulator
 
 
-def set_config(config):
-    simulation = _simulator.SimulationParameter(
-        config.total_num_people,
-        int(config.infected_people * config.total_num_people),
-        config.move_speed,
-        config.infect_rate,
-        config.mortality_rate,
-        config.recovery_rate,
-        config.healthcare_capacity,
-        config.left_x,
-        config.right_x,
-        config.up_y,
-        config.down_y,
-        config.simu_step,
-        config.mode,
-        config.move_step,
-        config.spread_range,
-        config.policy)
+def set_simulation_state(config) -> _simulator.SimulationState:
+    """
+    Put the parameters in config.py to init the simulation state.
+    """
+    simulation = _simulator.SimulationState(
+        config.TOTAL_POPULATION,
+        int(config.INFECTED_PEOPLE * config.TOTAL_POPULATION),
+        config.MOVE_SPEED,
+        config.INFECTED_RATE,
+        config.MORTALITY_RATE,
+        config.RECOVERY_RATE,
+        config.HEALTHCARE_CAPACITY,
+        config.LEFT_X,
+        config.RIGHT_X,
+        config.UP_Y,
+        config.DOWN_Y,
+        config.SIMULATION_STEP,
+        config.MODE,
+        config.MOVE_STEP,
+        config.SPREAD_RANGE,
+        config.POLICY)
+
     return simulation
 
 
+def save_result(simulation_state: _simulator.SimulationState, figure: plt.figure, fig1: plt.figure, fig2: plt.figure, count_pop: my_simulation.population, frames: int) -> None:
+    """
+    After the simulator is finished, save the result image in certain directory.
+    """
+
+    # Finished simulator, draw the result figure
+    my_simulation.draw_current_simulation_state(simulation_state, figure, fig1, fig2, count_pop, frames)
+
+    # Finished simulator, saved the result figure
+    dirname = './output_images'
+    os.makedirs(dirname, exist_ok = True)
+    plt.savefig(f'{dirname}/Simulation_policy{simulation_state.policy}.png')
+
+
+def on_each_iter_updated(simulation_state: _simulator.SimulationState, figure: plt.figure, fig1: plt.figure, fig2: plt.figure, count_pop: my_simulation.population, frames: int) -> None:
+    """
+    In each iteration, the current simulation state is drawn.
+    If the simulation state been changed, print current state of population in terminal
+    """
+    # visualize
+    if simulation_state.mode == 1:
+        my_simulation.draw_current_simulation_state(simulation_state, figure, fig1, fig2, count_pop, frames)
+        plt.draw()
+        plt.pause(0.0001)
+
+    if simulation_state.dirty == True:
+        my_simulation.print_in_terminal(frames, simulation_state)
+
+
+
+def main() -> None:
+    """
+    The main function running simulator.
+    1. init simulation state
+    2. init figure for visualization
+    3. run simulator
+    """
+    s = time.time()
+    # read all config from config.py file and ste init simulation state
+    simulation_state = set_simulation_state(config)
+
+    # init figure for visualization
+    figure, _, fig1, fig2 = my_simulation.build_figure(simulation_state.lx_bound, simulation_state.rx_bound, simulation_state.dy_bound, simulation_state.uy_bound, simulation_state.simulation_step, simulation_state.total_num_people)
+    e = time.time()
+    print(f'=== Initialize time: {e-s} ===')
+
+    # run simulator
+    if simulation_state.mode == 2:
+        # Store the results in a gif file.
+        s = time.time()
+        my_simulation.run_and_build_animation(simulation_state, figure, fig1, fig2)
+        e = time.time()
+        print(f'=== TOTAL TIME: {e-s}')
+    else:
+        my_simulation.run(simulation_state, figure, fig1, fig2, on_each_iter_updated, save_result)
+
+
+
 if __name__ == '__main__':
-    # read all config from config.py file
-    config = config()
-    simulation = set_config(config)
-    # doing simulation
-    my_simulation.simulate(simulation)
+    main()
--- my_simulation/simulator.py
+++ my_simulation/simulator.py
@@ -1,57 +1,57 @@
-from my_simulation.visualize import build_figure
-from my_simulation.visualize import draw_current_simu_status
-from my_simulation.visualize import run_and_build_animation
-from my_simulation.visualize import population
-from my_simulation.visualize import print_in_terminal
 from my_simulation.population import population
-import matplotlib.pyplot as plt
 import _simulator
-import numpy as np
-import sys
-import os
+from typing import Callable
+import matplotlib.pyplot as plt
+import time
 
 
-def run(simu_status):
-    if simu_status.mode == 2:
-        run_and_build_animation(simu_status)
-    else:
-        frames = 0
-        max_frames = simu_status.simu_step
+def run(simulation_state: _simulator.SimulationState, figure: plt.figure, fig1: plt.figure, fig2: plt.figure, on_each_iter_updated: Callable, save_result: Callable) -> None:
+    """
+    run the simulator and save the result.
 
-        # init figure
-        figure, spec, fig1, fig2 = build_figure(simu_status.lx_bound, simu_status.rx_bound, simu_status.dy_bound, simu_status.uy_bound, max_frames, simu_status.total_num_people)
+    _simulator.RunStep:
+        1. Check policy
+        2. Everyone move one step
+        3. Spread the virus: If someone is infected, update his/her state.
+        4. Recovered or dead: Check whether the infected person has recovered or died.
+        5. Classify people: For visualize.
+    """
+    frames = 0
+    max_frames = simulation_state.simulation_step
 
-        # for statistics graph
-        count_pop = population()
+    # for statistics graph
+    count_pop = population()
 
-        print_in_terminal(frames, simu_status)
-        while frames < max_frames:
-            if simu_status.num_infected == 0:
-                break
-            frames += 1
-            _simulator.RunStep(simu_status)
+    run_step_time = 0
+    pop_update_time = 0
+    visualize_time = 0
 
-            count_pop.update(simu_status.total_num_people, simu_status.num_infected, simu_status.num_recovered, simu_status.num_dead, frames)
+    while frames < max_frames:
+        if simulation_state.num_infected == 0:
+            break
+        frames += 1
 
-            # visualize
-            if simu_status.mode == 1:
-                draw_current_simu_status(simu_status, figure, fig1, fig2, count_pop, frames)
-                plt.draw()
-                plt.pause(0.0001)
-            if simu_status.dirty == True:
-                print_in_terminal(frames, simu_status)
-        draw_current_simu_status(simu_status, figure, fig1, fig2, count_pop, frames)
-        if not os.path.exists('./image'):
-            os.makedirs('./image')
-        plt.savefig('./image/Sim_policy%i.png' %simu_status.policy)
+        s = time.time()
+        # run one time step in c++ environment.
+        _simulator.RunStep(simulation_state)
+        e = time.time()
+        run_step_time += e - s
 
+        s = time.time()
+        count_pop.update(simulation_state.total_num_people, simulation_state.num_infected, simulation_state.num_recovered, simulation_state.num_dead, frames)
+        e = time.time()
+        pop_update_time += e - s
 
-def simulate(simu_status):
-    print("In")
+        s = time.time()
+        on_each_iter_updated(simulation_state, figure, fig1, fig2, count_pop, frames)
+        e = time.time()
+        visualize_time += e - s
 
-    # init
-    _simulator.InitSimulation(simu_status)
+    s = time.time()
+    save_result(simulation_state, figure, fig1, fig2, count_pop, frames)
+    e = time.time()
+    visualize_time += e - s
 
-    run(simu_status)
-
-    simu_status.print_status
+    print(f'=== Run step time: {run_step_time} ===')
+    print(f'=== Pop update time: {pop_update_time} ===')
+    print(f'=== visualize time: {visualize_time} ===')
